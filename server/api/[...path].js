b85adefb3baa8e58129efe0918fa819d789a1b20
IT2 <testnati95@gmail.com>
2026-07-04 10:28:48 +0300

    Intercept CORS preflight OPTIONS requests at serverless function level

--- server/api/[...path].js
+++ server/api/[...path].js
@@ -14,6 +14,18 @@ function isHealthCheck(req) {
 }
 
 export default async function handler(req, res) {
+  const origin = req.headers.origin;
+  if (origin) {
+    res.setHeader("Access-Control-Allow-Origin", origin);
+    res.setHeader("Access-Control-Allow-Credentials", "true");
+  }
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+
+  if (req.method === "OPTIONS") {
+    return res.status(204).end();
+  }
+
   normalizeRequestUrl(req);
 
   if (isHealthCheck(req)) {
